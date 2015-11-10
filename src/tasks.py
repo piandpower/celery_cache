638657d38342e334cbd172e1d50946f8eb9c5e70
Brittain Hard <brittainhard@gmail.com>
2015-11-10 16:30:39 -0600

    First pass at redis as cache.

--- src/tasks.py
+++ src/tasks.py
@@ -1,13 +1,29 @@
-from celery import Celery, shared_task
-
+from celery import Celery, Task, shared_task
+from celery.contrib import rdb
+import redis
+import time
 
 app = Celery()
 app.config_from_object('celeryconfig')
 
 
-@shared_task
-def cached_task(function, *args, **kwargs):
-    return function(*args, **kwargs)
+client = redis.StrictRedis(host='localhost', port=6379, db=1)
+
+
+class CachedTask(Task):
+    abstract = True
+
+    def after_return(self, status, retval, *args, **kwargs):
+        pass
+
+
+@shared_task(base=CachedTask)
+def cached_task(func, *args, **kwargs):
+    cached_result = client.get("%s %s %s" % (func.func_name, args, kwargs))
+    if cached_result:
+        return cached_result
+    else:
+        return func(*args, **kwargs)
 
 
 def add(a, b):

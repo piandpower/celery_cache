0197dc3a052674eb3653640bd082b3d921b4fae9
Brittain Hard <brittainhard@gmail.com>
2015-11-10 14:19:54 -0600

    app isntantiated without arguments.

--- src/celeryconfig.py
+++ src/celeryconfig.py
@@ -1,3 +1,4 @@
 BROKER_URL = "redis://localhost"
 CELERY_RESULT_BACKEND = "redis://localhost"
+
 CELERY_IMPORTS = ("tasks", )
--- src/tasks.py
+++ src/tasks.py
@@ -1,13 +1,14 @@
 from celery import Celery, shared_task
 
 
-app = Celery('tasks')
+app = Celery()
 app.config_from_object('celeryconfig')
 
 
 @shared_task
 def cached_task(function, *args, **kwargs):
     return function(*args, **kwargs)
+
 
 def add(a, b):
     return a + b

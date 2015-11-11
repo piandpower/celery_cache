d726f1e5376c0be44b1776ab9cfcbc1be0f7d435
Brittain Hard <brittainhard@gmail.com>
2015-11-11 09:30:48 -0600

    Added redis_key function and __init__ on arguments.

--- src/tasks.py
+++ src/tasks.py
@@ -3,11 +3,10 @@ from celery.contrib import rdb
 import redis
 import time
 
+
 app = Celery()
 app.config_from_object('celeryconfig')
-
-
-client = redis.StrictRedis(host='localhost', port=6379, db=1)
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 
 class CachedTask(Task):
@@ -16,11 +15,21 @@ class CachedTask(Task):
     def after_return(self, status, retval, *args, **kwargs):
         pass
 
+    def redis_key(self):
+        return str({
+            self.func.func_name:{
+                "args": self.func_args,
+                "kwargs": self.func_kwargs
+            }
+        })
+
 
 @shared_task(bind=True, base=CachedTask)
 def cached_task(self, func, *args, **kwargs):
     self.func = func
-    cached_result = client.get("%s %s %s" % (self.func.func_name, args, kwargs))
+    self.func_args = args
+    self.func_kwargs = kwargs
+    cached_result = redis_client.get(self.redis_key())
     if cached_result:
         return cached_result
     else:
